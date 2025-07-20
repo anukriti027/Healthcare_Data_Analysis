21ac78b81b292631ee40d3158a35ae8215c0891b
anukriti027 <136795845+anukriti027@users.noreply.github.com>
2025-07-20 18:34:51 +0530

    Update Healthare_SQL_Project.sql

--- Healthare_SQL_Project.sql
+++ Healthare_SQL_Project.sql
@@ -20,43 +20,58 @@ select datepart(mm,admission_date) as Month_of_2024, count(admission_id) as Tota
 from healthcare
 where datepart(yy,admission_date)='2024'
 group by datepart(mm,admission_date)
-order by Month_of_2024 
+order by Month_of_2024 ;
 
 --5.What is the average length of stay for patients grouped by their medical condition?
 select Medical_condition,avg(datediff(day,admission_date, discharge_date)) as Average_length_of_stay
 from healthcare
-group by medical_condition
+group by medical_condition;
 
 --6.What is the total billing amount per insurance provider?
 select Insurance_provider, sum(billing_amount) as Total_billing_amount
 from healthcare
-group by insurance_provider
+group by insurance_provider;
 
 --7.Who are the top 5 doctors by total billing amount?
 select top 5 Doctor, sum(billing_amount) as Total_billing_amount
 from healthcare
 group by doctor
-order by total_billing_amount desc
+order by total_billing_amount desc;
 
 --8.Which patients have been admitted more than once?
 select Name
 from healthcare
 group by Name
-having (count(admission_id)<> 1)
+having (count(admission_id)<> 1);
 
 --9.What is the average billing amount for each diagnosis (medical condition)?
 select Medical_condition, avg(billing_amount) as Avg_billing_amount
 from healthcare
-group by medical_condition
+group by medical_condition;
 
---10.Rank hospitals by their total billing amount using window functions.
+--10.Flag Readmissions Based on Length of Stay
+/*Question:
+Use CASE WHEN to label each patient record as:
+'Short Stay' if Length of Stay < 3 days
+'Normal Stay' if between 3 and 7 days
+'Long Stay' if > 7 days*/
+
+select Name, Admission_date, Discharge_date, 
+case when datediff(day, admission_date,discharge_date ) < 3 THEN 'Short'
+when datediff(day, admission_date,discharge_date) between 3 and 7 THEN 'Normal'
+when datediff(day, admission_date,discharge_date) > 7 THEN 'Long'
+end as Length_of_hospitalization
+from healthcare;
+
+--11.Rank hospitals by their total billing amount using window functions.
 select Hospital, 
 sum(billing_amount) as Total_billing_amount,
 rank() over(order by sum(billing_amount) desc) as Rank
 from healthcare
-group by hospital
+group by hospital;
 
---11.Which patients had a greater than 50% increase in average length of stay year-over-year?
+
+--12.Which patients had a greater than 50% increase in average length of stay year-over-year?
 with yearly_stay as (
     select name,
            datepart(year,Admission_date) as year,
@@ -79,7 +94,7 @@ from stay_diff
 where pct_increase > 0.5;
 
 
---12.Identify patients who were readmitted within 30 days of their previous discharge.
+--13.Identify patients who were readmitted within 30 days of their previous discharge.
 
 with cte as (select Name, admission_date,
 discharge_date,
@@ -97,21 +112,7 @@ select name,
 datediff(day, admission_date, prev_discharge_dt) as No_of_days_bw_hospitaization
 from prev
 where datediff(day, admission_date, prev_discharge_dt) >0
-and datediff(day, admission_date, prev_discharge_dt) < 30
-
---13.Flag Readmissions Based on Length of Stay
-/*Question:
-Use CASE WHEN to label each patient record as:
-'Short Stay' if Length of Stay < 3 days
-'Normal Stay' if between 3 and 7 days
-'Long Stay' if > 7 days*/
-
-select Name, Admission_date, Discharge_date, 
-case when datediff(day, admission_date,discharge_date ) < 3 THEN 'Short'
-when datediff(day, admission_date,discharge_date) between 3 and 7 THEN 'Normal'
-when datediff(day, admission_date,discharge_date) > 7 THEN 'Long'
-end as Length_of_hospitalization
-from healthcare
+and datediff(day, admission_date, prev_discharge_dt) < 30;
 
 
 
